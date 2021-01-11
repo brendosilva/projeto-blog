3edc49e2fc84ab2780e00b05a3d5ba53deed30e2
Brendo Silva <breendo.01@gmail.com>
2021-01-11 00:18:43 -0300

    Criação view de nova categoria

--- categories/categoriesControllers.js
+++ categories/categoriesControllers.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const slugify = require("slugify") // deixa a string em um modelo url
 const router = express.Router();
+const Categorie = require('./categorie');
 
-
-router.get("/categories", (req, res) => {
-    res.send("Categorias");
-});
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("Criação de novas categorias");
+    res.render("admin/categories/new");
 });
 
+router.post("/categories/save", (req, res) => {
+    let title = req.body.title_categorie;
+    if(title != undefined)
+    {
+        Categorie.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        });
+    }
+    else
+    {
+        res.redirect("/admin/categories/new");
+    }
+})
 module.exports = router;
