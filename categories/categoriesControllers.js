d41ec40b4f1fbbb135b330aecdafd9885f004dc0
Brendo Silva <breendo.01@gmail.com>
2021-01-11 01:58:06 -0300

    Adicionando funcionalidade delete

--- categories/categoriesControllers.js
+++ categories/categoriesControllers.js
@@ -8,6 +8,13 @@ router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 });
 
+router.get("/admin/categories", (req, res) => {
+    Categorie.findAll().then((categories) => {
+        res.render("admin/categories/index", { categories: categories });
+    })
+    
+})
+
 router.post("/categories/save", (req, res) => {
     let title = req.body.title_categorie;
     if(title != undefined)
@@ -24,4 +31,24 @@ router.post("/categories/save", (req, res) => {
         res.redirect("/admin/categories/new");
     }
 })
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+
+    if(id != undefined)
+    {
+        Categorie.destroy({
+            where: { id: id}
+        }).then(() => {
+            res.redirect("/admin/categories");
+        })
+    }    
+    else
+    {
+        res.redirect("/admin/categories");
+    }
+
+});
+
+
 module.exports = router;
